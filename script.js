586d2210d58c34263fee30b8b42014cc93da4677
snehalk5 <128291041+snehalk5@users.noreply.github.com>
2024-01-10 17:27:30 +0530

    Add files via upload
    Uploaded

---
+++ script.js
@@ -0,0 +1,26 @@
+let input = document.getElementById('inputBox');
+// will select the button text
+let buttons=document.querySelectorAll('button');
+let str = "";
+// button array
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            str = eval(str);
+            input.value = str;
+        }
+        else if (e.target.innerHTML == 'AC'){
+            str="";
+            input.value=str;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            str=str.substring(0,str.length-1);
+            input.value=str;
+        }
+        else{
+            str+=e.target.innerHTML;
+            input.value=str;
+        }
+    })
+})
